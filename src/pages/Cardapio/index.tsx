6690fc0ca21b97f4d81931731acd145eebc1c889
Jorge Francisco <Jorge7.9@hotmail.com>
2022-03-03 23:52:37 -0300

    feat: item listing

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import Buscador from './Buscador';
 import { Ordenador } from 'pages/Ordenador';
 import Filtros from './Filtros';
+import Itens from './Itens';
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
   const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState('');
   return (
     <main>
       <nav className={styles.menu}>
@@ -21,8 +23,9 @@ export default function Cardapio() {
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   );
--- src/pages/Ordenador/index.tsx
+++ src/pages/Ordenador/index.tsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
 import opcoes from './opcoes.json';
 import styles from './/Ordenador.module.scss';
+import classNames from 'classnames';
 
-export function Ordenador() {
+type Props = {
+  ordenador: string;
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export function Ordenador({ ordenador, setOrdenador }: Props) {
+  const [aberto, setAberto] = useState(false);
+  const nomeOrdenador =
+    ordenador && opcoes.find((opcao) => opcao.value === ordenador)?.nome;
   return (
-    <button className={styles.ordenador}>
-      <span>Ordenar Por</span>
-      <div className={styles.ordenador__options}>
+    <button
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles['ordenador--ativo']]: ordenador !== '',
+      })}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
+      <span>{nomeOrdenador || 'Ordenar Por'}</span>
+      {aberto ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles['ordenador__options--ativo']]: aberto,
+        })}
+      >
         {opcoes.map((opcao) => (
-          <div className={styles.ordenador__option} key={opcao.value}>
+          <div
+            className={styles.ordenador__option}
+            key={opcao.value}
+            onClick={() => setOrdenador(opcao.value)}
+          >
             {opcao.nome}
           </div>
         ))}
